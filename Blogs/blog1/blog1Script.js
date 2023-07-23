eee11b6a28843a7199d0bbc7fd3252917236dc80
ECS-news <139965503+ECS-news@users.noreply.github.com>
2023-07-23 13:08:54 +0100

    Update and rename blog1Script to blog1Script.js

---
+++ Blogs/blog1/blog1Script.js
@@ -0,0 +1,19 @@
+function Python () {
+  document.getElementById("python").style.display = "block";  
+  document.getElementById("java").style.display = "none";  
+  document.getElementById("javascript").style.display = "none";  
+  
+}
+function Java () {
+    document.getElementById("python").style.display = "none";  
+    document.getElementById("java").style.display = "block";  
+    document.getElementById("javascript").style.display = "none";
+  
+}
+function Javascript() {
+  document.getElementById("python").style.display = "none";  
+  document.getElementById("java").style.display = "none";  
+  document.getElementById("javascript").style.display = "block";
+  
+}
+
